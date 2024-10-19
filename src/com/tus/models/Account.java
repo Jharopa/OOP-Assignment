e30e6e71f5e2372cd1962bb1b2136d8560eaa8d3
Alex Burns <jharopa@gmail.com>
2024-10-19 13:55:55 +0100

    Updated string formatting

--- src/com/tus/models/Account.java
+++ src/com/tus/models/Account.java
@@ -2,9 +2,11 @@ package com.tus.models;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.tus.interfaces.Accountable;
+import com.tus.utils.StringUtils;
 
 public abstract class Account implements Accountable {
 	protected int id;
@@ -14,7 +16,7 @@ public abstract class Account implements Accountable {
 	private static AtomicInteger nextId = new AtomicInteger();
 	
 	public Account(double balance) {
-		this.id = nextId.getAndIncrement();
+		this.id = nextId.incrementAndGet();
 		this.balance = balance;
 		this.transactions = new  ArrayList<Transaction>();
 	}
@@ -36,5 +38,24 @@ public abstract class Account implements Accountable {
 
 	@Override
 	public void withdraw(double amount) {
+	}
+	
+	public void listTransactions() {
+		if (transactions.size() == 0) {
+			System.out.println("No transactions available");
+		}
+		else {
+			String header = StringUtils.makeHeader("Account " + id + " Transactions");
+			
+			System.out.println(header);
+			transactions.forEach(t -> System.out.println(t));
+		}
+	}
+	
+	public static Account findCustomerById(Collection<Account> accounts, Integer id) {
+		return accounts.stream()
+				.filter(a -> id.equals(a.getId()))
+				.findFirst()
+				.orElse(null);
 	}
 }
--- src/com/tus/models/Transaction.java
+++ src/com/tus/models/Transaction.java
@@ -2,16 +2,16 @@ package com.tus.models;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import com.tus.interfaces.Account;
+import com.tus.interfaces.Accountable;
 
 public record Transaction(LocalDate transactionDate, double amount, double balance) {
 	
 	@Override
 	public String toString() {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
 		
-		return "Transaction date: " + this.transactionDate.format(formatter) + "\t" + 
-				"Amount: " + this.amount + "\t" +
-				"Balance: " + this.balance + "\n";
+		return "Transaction date: " + this.transactionDate.format(formatter) + "    " + 
+				"Amount: " + this.amount + "    " +
+				"Balance: " + this.balance;
 	}
 }
